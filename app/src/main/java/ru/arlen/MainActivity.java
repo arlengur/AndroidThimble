31425fc86958f5ad2a659d8952f4fd0ebdde9e0b
angalin <ANGalin.SBT@sberbank.ru>
2018-12-14 19:44:54 +0300

    Looper added.

--- app/src/main/java/ru/arlen/MainActivity.java
+++ app/src/main/java/ru/arlen/MainActivity.java
@@ -2,6 +2,7 @@ package ru.arlen;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.ImageView;
 
@@ -24,14 +25,18 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
         newGameBtn = findViewById(R.id.btn);
         leftBox = findViewById(R.id.imageLeft);
+        leftBox.setTag(1);
         centerBox = findViewById(R.id.imageCenter);
+        centerBox.setTag(2);
         rightBox = findViewById(R.id.imageRight);
+        rightBox.setTag(3);
 
         View.OnClickListener onClickListener = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                int userChoice = (int) v.getTag();
                 gamePlayed = true;
-                showBall(ballPlace);
+                showBall(ballPlace, userChoice);
             }
         };
         leftBox.setOnClickListener(onClickListener);
@@ -52,8 +57,7 @@ public class MainActivity extends Activity {
         if (savedInstanceState != null && savedInstanceState.getBoolean(GAME_PLAYED)) {
             gamePlayed = savedInstanceState.getBoolean(GAME_PLAYED);
             ballPlace = savedInstanceState.getInt(BALL_PLACE);
-            showBall(ballPlace);
-
+            showBall(ballPlace, ballPlace);
         } else
             ballPlace = new Random().nextInt(2) + 1;
     }
@@ -68,17 +72,27 @@ public class MainActivity extends Activity {
         }
     }
 
-    private void showBall(int ballPlace) {
+    private void showBall(int ballPlace, int userChoice) {
+        long delay = ballPlace == userChoice ? 0 : 2000;
+        final ImageView view;
         switch (ballPlace) {
             case 1:
-                leftBox.getDrawable().setLevel(MAX_DRAWABLE_LEVEL);
+                view = leftBox;
                 break;
             case 2:
-                centerBox.getDrawable().setLevel(MAX_DRAWABLE_LEVEL);
+                view = centerBox;
                 break;
             case 3:
-                rightBox.getDrawable().setLevel(MAX_DRAWABLE_LEVEL);
+                view = rightBox;
                 break;
+            default:
+                throw new IllegalArgumentException("Невозможное положение шарика!");
         }
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                view.getDrawable().setLevel(MAX_DRAWABLE_LEVEL);
+            }
+        }, delay);
     }
 }
